20756a5015fd05bd8971313342f8906a2c924651
Laurie Fish <iamlauriefish@gmail.com>
2023-01-12 15:00:21 -0800

    started 2nd and 3rd functions

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { myFunction } from '../functions.js';
+import { myFunction, addExclamationPoints } from '../functions.js';
 
 const { test, skip } = QUnit;
 
@@ -11,10 +11,50 @@ test('this test should pass', (expect) => {
     expect.equal(actual, expected, 'true = true');
 });
 
-skip('this test should be skipped', (expect) => {
-    const expected = true;
+test('this test should add 3 exclamation points', (expect) => {
+    const expected = 'bunny rabbit!!!';
 
-    const actual = true;
+    const actual = addExclamationPoints('bunny rabbit');
 
     expect.equal(actual, expected);
+
+    const expected1 = 'hello world!!!';
+
+    const actual1 = addExclamationPoints('hello world');
+
+    expect.equal(actual1, expected1, 'true - true');
+
+    const expected2 = 'puppies!!!';
+
+    const actual2 = addExclamationPoints('puppies');
+
+    expect.equal(actual2, expected2, 'true - true');
+});
+
+test('function should take a number and multiply it by 7', (expect) => {
+    const expected = '28';
+
+    const actual = multiplyBySeven('4');
+
+    expect.equal(actual, expected, 'num, 7');
+
+    const expected1 = '28';
+
+    const actual1 = multiplyBySeven('num');
+
+    expect.equal(actual1, expected1, 'num, 7');
+
+    const expected2 = '28';
+
+    const actual2 = multiplyBySeven('num');
+
+    expect.equal(actual2, expected2, 'num, 7');
+});
+
+test('function should take in a number and multiply it by 12 then cut the result in half', (expect) => {
+    const expected = '10';
+
+    const actual = divideThenMultiply('8', '4', '5');
+
+    expect.equal(actual, expected, 'num, 7');
 });

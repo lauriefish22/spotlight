df2cdb974ec506c84c89ef3b919d6dfcba1a996a
Laurie Fish <iamlauriefish@gmail.com>
2023-01-13 14:18:22 -0800

    fixed getlastarray function

--- functions.js
+++ functions.js
@@ -146,7 +146,7 @@ Output:
 */
 
 export function getLastItem(array) {
-    return array[3];
+    return array[array.length - 1];
 }
 
 // This function should take a dog object and return an <li> with the name of the dog
